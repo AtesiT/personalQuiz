52fc652cd5f92d493092c414fd6f8484b9ae8d84
AtesiT <atesit@mail.ru>
2025-09-12 15:06:56 +0300

    added view controllers(on their buttons, labels, tollbars etc.) without stacks

---
+++ personalQuiz/App/LastViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class LastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
--- personalQuiz/ViewControllers/ViewController.swift
+++ personalQuiz/ViewControllers/ViewController.swift
@@ -1,10 +1,12 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()    }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
 }
 
